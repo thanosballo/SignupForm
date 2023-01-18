182fa6716004e1e92713e54360c1aedf0189709b
thanosbalo <thanosbalogiannis@gmail.com>
2023-01-18 16:36:39 +0200

    changes made

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const button=document.querySelector("button");
 
+const conpass=document.querySelector("#con_password");
+conpass.addEventListener("keyup",()=>{
+    let pass=document.querySelector("#password").value;
+    let conpassval=document.querySelector("#con_password").value;
+    if (pass===conpassval){
+        console.log("valid",pass,conpassval)
+    } else {
+        console.log("invalid",pass,conpassval)
+    }
+})
